85df2e86025c712f6495e8556b685f436163c709
DreGonGith <136021554@qq.com>
2019-04-15 08:39:27 +0800

    init shop

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/*
+ * 入口js
+*/
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router
+  // render: h => h(App)
+})
